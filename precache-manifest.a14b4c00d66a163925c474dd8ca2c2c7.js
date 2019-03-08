ecff28fcf13756e42ba7ea0f62a3ee750ea46eda
suman <suman.kunwar@daliaresearch.com>
2019-03-08 23:55:31 +0100

    Updates

---
+++ precache-manifest.a14b4c00d66a163925c474dd8ca2c2c7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9893a21f1888b6f057e",
+    "url": "/shopping-cart/static/js/runtime~main.6ebcdd6e.js"
+  },
+  {
+    "revision": "9f65bfc1a5e86cee3371",
+    "url": "/shopping-cart/static/js/main.2693670b.chunk.js"
+  },
+  {
+    "revision": "8038dd300fa37dfc68f4",
+    "url": "/shopping-cart/static/js/2.a22c8327.chunk.js"
+  },
+  {
+    "revision": "5a0ea4a099d5913f264e642e7b87d3e0",
+    "url": "/shopping-cart/index.html"
+  }
+];
